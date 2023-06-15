b9afc2234c96d555acbdfce06f3d6ebc5cffbd25
Eric Hou <eric.hou@aspiraconnect.com>
2023-06-15 09:15:35 +0000

    AWO-209754 PCRD624 - CA App - Multi-language spike

--- hf-ca/App.js
+++ hf-ca/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
+import { I18nextProvider } from "react-i18next";
 import RootScreen from "./src/screens/RootScreen";
 import store from "./src/redux/Store";
 import AppContract from "./src/assets/_default/AppContract";
 import { initAppConfig } from "./src/services/AppConfigService";
+import i18n from "./src/localization/i18n";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -29,8 +31,10 @@ export default function App() {
         return null;
     }
     return (
-        <Provider store={store}>
-            <RootScreen />
-        </Provider>
+        <I18nextProvider i18n={i18n}>
+            <Provider store={store}>
+                <RootScreen />
+            </Provider>
+        </I18nextProvider>
     );
 }
