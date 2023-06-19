38481488aacbbec5adcd0fb0185e568a3d49e99c
Charles Xi <charles.xi@aspiraconnect.com>
2023-06-20 00:03:53 +0800

    AWO-210645 DEV - Coding for Error Message/Code Refactor

--- hf-ca/src/screens/profile/add_profile_screen/BusinessProfileInfo.js
+++ hf-ca/src/screens/profile/add_profile_screen/BusinessProfileInfo.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useImperativeHandle, useRef } from "react";
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { genTestId } from "../../../helper/AppHelper";
@@ -6,15 +6,26 @@ import StatefulTextInput from "../../../components/StatefulTextInput";
 import AppTheme from "../../../assets/_default/AppTheme";
 import { emptyError, emptyValidate } from "./ProfileValidate";
 
-function BusinessProfileInfo({ profile, setProfile }) {
+const BusinessProfileInfo = React.forwardRef(({ profile, setProfile }, ref) => {
     const { t } = useTranslation();
     const goIDNumberRef = useRef();
     const postalCodeNumberRef = useRef();
+    const validate = () => {
+        const errorOfGOIDNumber = emptyValidate(profile?.goIDNumber, t("errMsg.emptyBusinessGOIDNumber"));
+        goIDNumberRef?.current.setError(errorOfGOIDNumber);
+        const errorOfPostalCodeNumber = emptyValidate(profile?.postalCodeNumber, t("errMsg.emptyPostalCodeNumber"));
+        postalCodeNumberRef?.current.setError(errorOfPostalCodeNumber);
+        return errorOfGOIDNumber.error || errorOfPostalCodeNumber.error;
+    };
+    useImperativeHandle(ref, () => ({
+        validate,
+    }));
     return (
         <View>
             <StatefulTextInput
                 testID={genTestId("BusinessGOIDNumberInput")}
                 label={t("profile.businessGOIDNumber")}
+                hint={t("common.pleaseEnter")}
                 ref={goIDNumberRef}
                 style={{ marginTop: 20 }}
                 labelStyle={{ color: AppTheme.colors.font_color_1 }}
@@ -32,6 +43,7 @@ function BusinessProfileInfo({ profile, setProfile }) {
             <StatefulTextInput
                 testID={genTestId("PostalCodeNumberInput")}
                 label={t("profile.postalCodeNumber")}
+                hint={t("common.pleaseEnter")}
                 ref={postalCodeNumberRef}
                 style={{ marginTop: 20 }}
                 labelStyle={{ color: AppTheme.colors.font_color_1 }}
@@ -48,6 +60,6 @@ function BusinessProfileInfo({ profile, setProfile }) {
             />
         </View>
     );
-}
+});
 
 export default BusinessProfileInfo;
--- hf-ca/src/screens/profile/add_profile_screen/IdentificationTypeSelector.js
+++ hf-ca/src/screens/profile/add_profile_screen/IdentificationTypeSelector.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useImperativeHandle } from "react";
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { genTestId } from "../../../helper/AppHelper";
@@ -6,119 +6,153 @@ import PopupDropdown from "../../../components/PopupDropdown";
 import StatefulTextInput from "../../../components/StatefulTextInput";
 import AppTheme from "../../../assets/_default/AppTheme";
 import { getCountriesStates } from "../../../services/ProfileService";
+import { emptyError, emptyValidate } from "./ProfileValidate";
+import { IDENTIFICATION_TYPE_GO_ID } from "../../../constants/Constants";
 
-function IdentifierTypeSelector({ identificationTypes, identificationType, handleIdentificationType }) {
-    const { t } = useTranslation();
-    const idTypeNames = identificationTypes ? identificationTypes.map((idType) => idType.name) : [];
+const IdentifierTypeSelector = React.forwardRef(
+    ({ identificationTypes, identificationType, handleIdentificationType, identificationTypeChanged }, ref) => {
+        const { t } = useTranslation();
+        const idNumberLabel =
+            identificationType?.id === IDENTIFICATION_TYPE_GO_ID
+                ? t("profile.identificationTypeGOID")
+                : t("profile.identificationNumber");
+        const identificationTypeRef = useRef();
+        const idNumberRef = useRef();
+        const idTypeNames = identificationTypes ? identificationTypes.map((idType) => idType.name) : [];
 
-    const findIdentificationConfig = (idType) => {
-        const { config } = identificationTypes?.find((type) => idType && type.id === idType.id) || {};
-        return config || {};
-    };
-    const initIdentificationConfig = findIdentificationConfig(identificationType);
-    const [currentIdentificationConfig, setCurrentIdentificationConfig] = useState(initIdentificationConfig);
+        const findIdentificationConfig = (idType) => {
+            const { config } = identificationTypes?.find((type) => idType && type.id === idType.id) || {};
+            return config || {};
+        };
+        const initIdentificationConfig = findIdentificationConfig(identificationType);
+        const [currentIdentificationConfig, setCurrentIdentificationConfig] = useState(initIdentificationConfig);
 
-    const changeIdentificationType = (index) => {
-        const selectedIdentificationType = identificationTypes[index];
-        handleIdentificationType(selectedIdentificationType);
-        const identificationConfig = findIdentificationConfig(selectedIdentificationType);
-        setCurrentIdentificationConfig(identificationConfig);
-    };
-    const { identificationInfo } = identificationType || {};
+        const { identificationInfo } = identificationType || {};
 
-    const [countriesStates, setCountriesStates] = useState();
+        const [countriesStates, setCountriesStates] = useState();
 
-    const { countries, states } = countriesStates || {};
+        const { countries, states } = countriesStates || {};
 
-    const issuedCountriesNames = countries ? countries.map((country) => country.name) : [];
-    const issuedStatesNames = states ? states.map((state) => state.name) : [];
+        const issuedCountriesNames = countries ? countries.map((country) => country.name) : [];
+        const issuedStatesNames = states ? states.map((state) => state.name) : [];
 
-    const changeCountryIssued = (index) => {
-        const selectedCountryIssued = countries[index];
-        handleIdentificationType({
-            ...identificationType,
-            identificationInfo: { ...identificationInfo, countryIssued: { ...selectedCountryIssued } },
-        });
-    };
-    const changeStateIssued = (index) => {
-        const selectedStateIssued = states[index];
-        handleIdentificationType({
-            ...identificationType,
-            identificationInfo: { ...identificationInfo, stateIssued: { ...selectedStateIssued } },
-        });
-    };
-    const changeIdNumber = (idNumber) => {
-        handleIdentificationType({
-            ...identificationType,
-            identificationInfo: { ...identificationInfo, idNumber },
-        });
-    };
+        const changeCountryIssued = (index) => {
+            const selectedCountryIssued = countries[index];
+            handleIdentificationType({
+                ...identificationType,
+                identificationInfo: { ...identificationInfo, countryIssued: { ...selectedCountryIssued } },
+            });
+        };
+        const changeStateIssued = (index) => {
+            const selectedStateIssued = states[index];
+            handleIdentificationType({
+                ...identificationType,
+                identificationInfo: { ...identificationInfo, stateIssued: { ...selectedStateIssued } },
+            });
+        };
+        const changeIdNumber = (idNumber) => {
+            handleIdentificationType({
+                ...identificationType,
+                identificationInfo: { ...identificationInfo, idNumber },
+            });
+        };
 
-    const getCountriesStatesData = async () => {
-        const countriesStatesData = await getCountriesStates();
-        setCountriesStates(countriesStatesData);
-    };
-    useEffect(() => {
-        getCountriesStatesData();
-    }, []);
-    return (
-        <View>
-            <PopupDropdown
-                testID={genTestId("IdentificationTypeDropdown")}
-                label={t("profile.identificationType")}
-                containerStyle={{ marginTop: 20 }}
-                valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
-                labelStyle={{ color: AppTheme.colors.font_color_1 }}
-                options={idTypeNames}
-                defaultValue={identificationType?.name}
-                onSelect={(index) => changeIdentificationType(index)}
-            />
-            {currentIdentificationConfig.issuedCountryRequired && (
-                <PopupDropdown
-                    testID={genTestId("CountryIssuedDropdown")}
-                    label={t("profile.countryIssued")}
-                    containerStyle={{ marginTop: 20 }}
-                    valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
-                    labelStyle={{ color: AppTheme.colors.font_color_1 }}
-                    options={issuedCountriesNames}
-                    defaultValue={identificationInfo?.countryIssued?.name}
-                    onSelect={(index) => changeCountryIssued(index)}
-                />
-            )}
-            {currentIdentificationConfig.issuedStateRequired && (
-                <PopupDropdown
-                    testID={genTestId("StateIssuedDropdown")}
-                    label={t("profile.stateIssued")}
-                    containerStyle={{ marginTop: 20 }}
-                    valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
-                    labelStyle={{ color: AppTheme.colors.font_color_1 }}
-                    options={issuedStatesNames}
-                    defaultValue={identificationInfo?.stateIssued?.name}
-                    onSelect={(index) => changeStateIssued(index)}
-                />
-            )}
-            {currentIdentificationConfig.idNumberRequired && (
-                <StatefulTextInput
-                    testID={genTestId("IdentificationNumberDropdown")}
-                    label={t("profile.identificationNumber")}
-                    style={{ marginTop: 20 }}
-                    labelStyle={{ color: AppTheme.colors.font_color_1 }}
-                    inputStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
-                    onChangeText={(text) => {
-                        // this.setState({ identifierNum: text });
-                        // const error = {
-                        //     error: false,
-                        //     errorMsg: null,
-                        // };
-                        // this.identifierNumRef?.current.setError(error);
-                        changeIdNumber(text);
-                    }}
-                    value={identificationInfo?.idNumber}
-                    onBlur={() => {}}
-                />
-            )}
-        </View>
-    );
-}
+        const getCountriesStatesData = async () => {
+            const countriesStatesData = await getCountriesStates();
+            setCountriesStates(countriesStatesData);
+        };
+        const idNumberError =
+            identificationType?.id === IDENTIFICATION_TYPE_GO_ID
+                ? t("errMsg.emptyIdentificationGOID")
+                : t("errMsg.emptyIdentificationNumber");
+        const validate = () => {
+            const errorOfIdentificationType =
+                identificationType?.id === "-1"
+                    ? { error: true, errorMsg: t("errMsg.emptyIdentificationType") }
+                    : { error: false };
+            identificationTypeRef?.current?.setError(errorOfIdentificationType);
+
+            const errorOfIDNumber = emptyValidate(identificationInfo?.idNumber, idNumberError);
+            idNumberRef?.current?.setError(errorOfIDNumber);
+
+            return errorOfIDNumber.error;
+        };
+        const changeIdentificationType = (index) => {
+            const selectedIdentificationType = identificationTypes[index];
+            handleIdentificationType(selectedIdentificationType);
+            const identificationConfig = findIdentificationConfig(selectedIdentificationType);
+            setCurrentIdentificationConfig(identificationConfig);
+            identificationTypeRef?.current?.setError(emptyError);
+            idNumberRef?.current?.setError(emptyError);
+        };
+        useImperativeHandle(ref, () => ({
+            validate,
+        }));
+        useEffect(() => {
+            getCountriesStatesData();
+        }, []);
+        return (
+            <View>
+                {identificationTypeChanged && (
+                    <PopupDropdown
+                        ref={identificationTypeRef}
+                        testID={genTestId("IdentificationTypeDropdown")}
+                        label={t("profile.identificationType")}
+                        containerStyle={{ marginTop: 20 }}
+                        valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
+                        labelStyle={{ color: AppTheme.colors.font_color_1 }}
+                        options={idTypeNames}
+                        defaultValue={t("profile.selectOne")}
+                        onSelect={(index) => changeIdentificationType(index)}
+                    />
+                )}
+                {currentIdentificationConfig.issuedCountryRequired && (
+                    <PopupDropdown
+                        testID={genTestId("CountryIssuedDropdown")}
+                        label={t("profile.countryIssued")}
+                        containerStyle={{ marginTop: 20 }}
+                        valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
+                        labelStyle={{ color: AppTheme.colors.font_color_1 }}
+                        options={issuedCountriesNames}
+                        defaultValue={identificationInfo?.countryIssued?.name || issuedCountriesNames[0]}
+                        onSelect={(index) => changeCountryIssued(index)}
+                    />
+                )}
+                {currentIdentificationConfig.issuedStateRequired && (
+                    <PopupDropdown
+                        testID={genTestId("StateIssuedDropdown")}
+                        label={t("profile.stateIssued")}
+                        containerStyle={{ marginTop: 20 }}
+                        valueContainerStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
+                        labelStyle={{ color: AppTheme.colors.font_color_1 }}
+                        options={issuedStatesNames}
+                        defaultValue={identificationInfo?.stateIssued?.name || issuedStatesNames[0]}
+                        onSelect={(index) => changeStateIssued(index)}
+                    />
+                )}
+                {currentIdentificationConfig.idNumberRequired && (
+                    <StatefulTextInput
+                        testID={genTestId("IdentificationNumberDropdown")}
+                        label={idNumberLabel}
+                        ref={idNumberRef}
+                        hint={t("common.pleaseEnter")}
+                        style={{ marginTop: 20 }}
+                        labelStyle={{ color: AppTheme.colors.font_color_1 }}
+                        inputStyle={{ backgroundColor: AppTheme.colors.font_color_4 }}
+                        onChangeText={(text) => {
+                            changeIdNumber(text);
+                            idNumberRef.current.setError(emptyError);
+                        }}
+                        value={identificationInfo?.idNumber}
+                        onBlur={() => {
+                            const error = emptyValidate(identificationInfo?.idNumber, idNumberError);
+                            idNumberRef.current.setError(error);
+                        }}
+                    />
+                )}
+            </View>
+        );
+    }
+);
 
 export default IdentifierTypeSelector;
