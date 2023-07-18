e086ccc6f42d9093a590bb1c28105a62c59c5847
Nathan Du <nathan.du@aspiraconnect.com>
2023-07-18 05:47:50 +0000

    AWO-211273 Global dialog definition refactoring

--- hf-ca/src/navigation/ModalScreen.js
+++ hf-ca/src/navigation/ModalScreen.js
@@ -1,42 +1,54 @@
+import { Modal } from "react-native";
 import { SimpleDialogView, SelectDialogView } from "../components/Dialog";
 import { DialogActions } from "../helper/DialogHelper";
 import NavigationService from "./NavigationService";
 
 export default function ModalScreen({ route = {} }) {
     const { params = {} } = route;
-    const { title, message, okText, cancelText, isSelect = false } = params;
-    if (!isSelect) {
+    const { title, message, okText, cancelText, isSelect = false, withModal = false } = params;
+
+    const Content = () => {
+        if (!isSelect) {
+            return (
+                <SimpleDialogView
+                    testID="globalSimpleDialog"
+                    title={title}
+                    message={message}
+                    okText={okText}
+                    okAction={() => {
+                        NavigationService.back();
+                        const act = DialogActions.okAction;
+                        act?.();
+                    }}
+                />
+            );
+        }
         return (
-            <SimpleDialogView
-                testID="globalSimpleDialog"
+            <SelectDialogView
+                testID="globalSelectDialog"
                 title={title}
                 message={message}
                 okText={okText}
+                cancelText={cancelText}
+                cancelAction={() => {
+                    NavigationService.back();
+                    const cancel = DialogActions.selCancelAction;
+                    cancel?.();
+                }}
                 okAction={() => {
                     NavigationService.back();
-                    const act = DialogActions.okAction;
-                    act?.();
+                    const ok = DialogActions.selOkAction;
+                    ok?.();
                 }}
             />
         );
+    };
+    if (!withModal) {
+        return <Content />;
     }
     return (
-        <SelectDialogView
-            testID="globalSelectDialog"
-            title={title}
-            message={message}
-            okText={okText}
-            cancelText={cancelText}
-            cancelAction={() => {
-                NavigationService.back();
-                const cancel = DialogActions.selCancelAction;
-                cancel?.();
-            }}
-            okAction={() => {
-                NavigationService.back();
-                const ok = DialogActions.selOkAction;
-                ok?.();
-            }}
-        />
+        <Modal animationType="none" transparent>
+            <Content />
+        </Modal>
     );
 }
