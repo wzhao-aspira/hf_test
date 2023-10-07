c4667c8054d8f12529eef3af95b63bcfd9065e41
Rick Hou <rick.hou@aspiraconnect.com>
2023-10-07 05:44:58 +0000

    Merge branch 'FEAT/AWO-214900' into 'Release_1.2.0'
    AWO-214900 HF CA - Prompt a network offline toast on dashboard and license list in offline mode without auto refresh time duration met.
    
    See merge request aspira-mobile/huntfish-ca!299
    
    (cherry picked from commit ce98a5a6c833675da11c4ea291c02902e6f0d940)
    
    f4499091 AWO-214900 HF CA - Prompt a network offline toast on dashboard and license...
    d67e9a9d AWO-000000 clear license data update time when go to purchase license
    44e5eba9 AWO-000000 update text label

--- hf-ca/src/screens/licenses/hooks/useNavigateToISPurchaseLicense.ts
+++ hf-ca/src/screens/licenses/hooks/useNavigateToISPurchaseLicense.ts
@@ -1,18 +1,22 @@
-import { useAppDispatch } from "../../../hooks/redux";
 import useNavigateToIS from "../../../hooks/useNavigateToIS";
-
+import { useAppDispatch } from "../../../hooks/redux";
 import { actions as licenseActions } from "../../../redux/LicenseSlice";
 
 function useNavigateToISPurchaseLicense() {
     const dispatch = useAppDispatch();
+
     const { navigateToIS } = useNavigateToIS();
 
     return {
-        navigateToIS: () => {
-            dispatch(licenseActions.clearUpdateTime());
-            navigateToIS({
+        navigateToIS: async () => {
+            const result = await navigateToIS({
                 targetPath: "/Sales/ItemSelection",
             });
+
+            if (result) {
+                // clear license update time
+                dispatch(licenseActions.clearUpdateTime());
+            }
         },
     };
 }
