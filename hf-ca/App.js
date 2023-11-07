0cc35ac32055a19927eccccf4a2ce9f70a745f59
Aspira\schen <shuang.chen@aspiraconnect.com>
2023-11-07 10:00:58 +0800

    AWO-216352:Show primary account when back in app.

--- hf-ca/App.js
+++ hf-ca/App.js
@@ -45,7 +45,7 @@ const checkLogin = async () => {
         const hasAccessToken = await restoreToken(lastUsedMobileAccountId);
         if (hasAccessToken) {
             await store.dispatch(appThunkActions.initUserData({ userID: lastUsedMobileAccountId }));
-            await store.dispatch(ProfileThunk.initProfile(false, true));
+            await store.dispatch(ProfileThunk.initProfile(false));
             store.dispatch(updateLoginStep(LoginStep.home));
             return;
         }
