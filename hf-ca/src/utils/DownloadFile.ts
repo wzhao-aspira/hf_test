9dfa4dff9e95a61c3e0d5375e5261c76c2f236e0
jyang1-aspira <141092946+jyang1-aspira@users.noreply.github.com>
2025-03-17 17:20:44 +0800

    AFB-38922 CA-11357 Optimize DownloadFile logic

--- hf-ca/src/utils/DownloadFile.ts
+++ hf-ca/src/utils/DownloadFile.ts
@@ -2,43 +2,48 @@ import * as FileSystem from 'expo-file-system';
 import { formatDownloadURL } from "../screens/regulation/detail/hooks/useFileOperations";
 import contentDispositionParser from "./contentDispositionParser";
 
-const downloadFile = async ({ url, folder, onStart, onComplete, onError }: { url: string, folder: string, onStart: () => void, onComplete: () => void, onError: () => void }) => {
+const downloadFile = async ({ url, folder, onStart, onComplete, onError }: { url: string, folder: string, onStart: () => void, onComplete: (downloadResult: FileSystem.FileSystemDownloadResult, folderUri: string) => void, onError: () => void }) => {
     try {
-        if (onStart) {
-            onStart();
-        }
+        onStart?.();
 
         const formattedDownloadURL = formatDownloadURL(url);
-        const folderUri = `${FileSystem.documentDirectory}${folder}/${formattedDownloadURL}/`;
-        await FileSystem.makeDirectoryAsync(folderUri, { intermediates: true });
-        const tempFileName = url?.split("/")?.pop();
-        const tempDestinationUri = folderUri + tempFileName;
+        const tempFolderUri = `${FileSystem.documentDirectory}temp${folder}/${formattedDownloadURL}/`;
+        await FileSystem.makeDirectoryAsync(tempFolderUri, { intermediates: true });
+        const tempFileName = "tempfile";
+        const tempDestinationUri = tempFolderUri + tempFileName;
         const result = await FileSystem.downloadAsync(url, tempDestinationUri);
         if (result.status !== 200) {
-            if (onError) {
-                onError();
-            }
+            onError?.();
             return;
         }
+
+        const folderUri = `${FileSystem.documentDirectory}${folder}/${formattedDownloadURL}/`;
+        //Delete previously downloaded files   
+        const preFileDirectory = await FileSystem.getInfoAsync(folderUri);
+        if (preFileDirectory.exists) {
+            await FileSystem.deleteAsync(folderUri);
+            console.log("Previously downloaded files deleted successfully");
+        }
+        await FileSystem.makeDirectoryAsync(folderUri, { intermediates: true });
+
         const contentDispositionValue = result.headers?.["Content-Disposition"] as string;
-        const fileName =
+        let fileName =
             // @ts-expect-error
             contentDispositionValue && contentDispositionParser(contentDispositionValue)?.filename;
-        if (!!fileName) {
-            const destinationUri = folderUri + fileName;
-            await FileSystem.moveAsync({
-                from: encodeURI(tempDestinationUri),
-                to: encodeURI(destinationUri),
-            });
+        if (!fileName) {
+            fileName = url?.split("/")?.pop();
         }
 
-        if (onComplete) {
-            onComplete();
-        }
+        const destinationUri = folderUri + fileName;
+        await FileSystem.moveAsync({
+            from: encodeURI(tempDestinationUri),
+            to: encodeURI(destinationUri),
+        });
+
+        onComplete?.(result, folderUri);
+
     } catch (error) {
-        if (onError) {
-            onError();
-        }
+        onError?.();
     }
 };
 
